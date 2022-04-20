603028d4db2ae64b46f794b31dbbcc18b2234b14
Richard Ingwe <richardingwe5@gmail.com>
2022-04-20 17:02:51 +0100

    add renderHeader

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,98 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import React from 'react';
+import { COLORS, FONTS, icons, SIZES } from "../constants";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Home = () => {
+    function renderHeader() {
+        return (
+            <View style={{
+                flexDirection: 'row',
+                height: 50,
+                alignItems: 'center',
+                marginTop: 10
+            }}>
+                <TouchableOpacity
+                    style={{
+                        width: 50,
+                        paddingLeft: SIZES.padding * 2,
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Image
+                        source={icons.nearby}
+                        resizeMode='contain'
+                        style={{
+                            width: 30,
+                            height: 30
+                        }}
+                    />
+                </TouchableOpacity>
+
+                <View style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}>
+                    <View
+                        style={{
+                            width: '70%',
+                            height: '100%',
+                            backgroundColor: COLORS.lightGray3,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            borderRadius: SIZES.radius
+                        }}
+                    >
+                        <Text
+                            style={{ ...FONTS.h3 }}
+                        >Location</Text>
+                    </View>
+                </View>
+
+                <TouchableOpacity
+                    style={{
+                        width: 50,
+                        paddingRight: SIZES.padding * 2,
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Image
+                        source={icons.basket}
+                        resizeMode='contain'
+                        style={{
+                            width: 30,
+                            height: 30
+                        }}
+                    />
+                </TouchableOpacity>
+            </View>
+        );
+    }
+
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            {renderHeader()}
+        </SafeAreaView>
     );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.lightGray4
+    },
+    shadow: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+        elevation: 1,
+    }
+});
