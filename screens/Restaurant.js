7387ff681a13ed362907253b5f63aee54b49a9e0
Richard Ingwe <richardingwe5@gmail.com>
2022-04-21 23:47:01 +0100

    renderHeader

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -8,18 +8,98 @@ import {
     Image,
     Animated
 } from "react-native";
-import { isIphoneX } from 'react-native-iphone-x-helper'
+import { isIphoneX } from 'react-native-iphone-x-helper';
 
-import { icons, COLORS, SIZES, FONTS } from '../constants'
+import { icons, COLORS, SIZES, FONTS } from '../constants';
 
-const Restaurant = () => {
+const Restaurant = ({ route, navigation }) => {
+    const scrollX = new Animated.Value(0);
+    const [restaurant, setRestaurant] = React.useState(null);
+    const [currentLocation, setCurrentLocation] = React.useState(null);
+    const [orderItems, setOrderItems] = React.useState([]);
+
+    React.useEffect(() => {
+        let { item, currentLocation } = route.params;
+
+        setRestaurant(item);
+        setCurrentLocation(currentLocation);
+    });
+
+    function renderHeader() {
+        return (
+            <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                <TouchableOpacity
+                    style={{
+                        width: 50,
+                        paddingLeft: SIZES.padding * 2,
+                        justifyContent: 'center'
+                    }}
+                    onPress={() => navigation.goBack()}
+                >
+                    <Image
+                        source={icons.back}
+                        resizeMode="contain"
+                        style={{
+                            width: 30,
+                            height: 30
+                        }}
+                    />
+                </TouchableOpacity>
+
+                {/* Restaurant Name Section */}
+                <View
+                    style={{
+                        flex: 1,
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <View
+                        style={{
+                            height: 50,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            paddingHorizontal: SIZES.padding * 3,
+                            borderRadius: SIZES.radius,
+                            backgroundColor: COLORS.lightGray3
+                        }}
+                    >
+                        <Text style={{ ...FONTS.h3 }}>{restaurant?.name}</Text>
+                    </View>
+                </View>
+
+                <TouchableOpacity
+                    style={{
+                        width: 50,
+                        paddingRight: SIZES.padding * 2,
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Image
+                        source={icons.list}
+                        resizeMode="contain"
+                        style={{
+                            width: 30,
+                            height: 30
+                        }}
+                    />
+                </TouchableOpacity>
+            </View>
+        );
+    }
+
     return (
-        <View>
-            <Text>Restaurant</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            {renderHeader()}
+        </SafeAreaView>
     );
 };
 
 export default Restaurant;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.lightGray2
+    }
+});
