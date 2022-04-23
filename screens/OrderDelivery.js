8afc0c1da99bf71464419891409e4e04d3ac4117
Richard Ingwe <richardingwe5@gmail.com>
2022-04-23 22:13:00 +0100

    add states

--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -12,6 +12,18 @@ import { COLORS, FONTS, icons, SIZES, GOOGLE_API_KEY } from "../constants";
 
 const OrderDelivery = () => {
 
+    const mapView = React.useRef()
+
+    const [restaurant, setRestaurant] = React.useState(null)
+    const [streetName, setStreetName] = React.useState("")
+    const [fromLocation, setFromLocation] = React.useState(null)
+    const [toLocation, setToLocation] = React.useState(null)
+    const [region, setRegion] = React.useState(null)
+
+    const [duration, setDuration] = React.useState(0)
+    const [isReady, setIsReady] = React.useState(false)
+    const [angle, setAngle] = React.useState(0)
+
     function renderMap() {
         return (
             <View style={{ flex: 1 }}>
